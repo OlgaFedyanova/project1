fbaddf79ca423edc2405690cb10545dddd3747a5
Olga <ofedyanova@gmail.com>
2020-07-24 01:04:47 +0300

    add new changes

--- js/script.js
+++ js/script.js
@@ -5,5 +5,23 @@
 // const result = confirm('Are you here?');
 // console.log(result);
 
-const age = +prompt('Are you 18', '18');
-console.log(age + 5);
+// const age = +prompt('Are you 18', '18');
+// console.log(age + 5);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+const a = prompt('один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b; //запись свойства b в объект 
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
